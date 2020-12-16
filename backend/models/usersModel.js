c6938a79ff50a4ca255401a8457acc209b9533c9
GWilczyk <gwilczyk.github@gmail.com>
2020-12-16 13:40:01 +0100

    Add userSchema Authentication Virtual Fields and Methods

--- backend/models/usersModel.js
+++ backend/models/usersModel.js
@@ -53,6 +53,42 @@ const userSchema = mongoose.Schema(
 	}
 );
 
+userSchema.methods = {
+	authenticate: function (plainPassword) {
+		return this.encryptPassword(plainPassword) === this.hashedPassword;
+	},
+	encryptPassword: function (password) {
+		if (!password) {
+			return '';
+		}
+		try {
+			return crypto
+				.createHmac('sha1', this.salt)
+				.update(password)
+				.digest('hex');
+		} catch (error) {
+			return '';
+		}
+	},
+	makeSalt: function () {
+		return Math.round(new Date().valueOf() * Math.random()) + '';
+	},
+};
+
+userSchema
+	.virtual('password')
+	.set(function (password) {
+		// Create temporary _password variable
+		this._password = password;
+		// Generate salt
+		this.salt = this.makeSalt();
+		// Encrypt password
+		this.hashedPassword = this.encryptPassword(password);
+	})
+	.get(function () {
+		return this._password;
+	});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
