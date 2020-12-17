3994889f9d792c281815ac61ecc331d41993d85d
GWilczyk <gwilczyk.github@gmail.com>
2020-12-17 11:40:04 +0100

    Reactstrap Navbar

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "88a5cccf3d7f54f4b5aa";
+/******/ 	var hotCurrentHash = "799ca7ab3146c4b8ddda";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
