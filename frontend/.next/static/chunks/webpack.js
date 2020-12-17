585b0c0cbdaf6bfe88d7d5cf4f760905faa8e65d
GWilczyk <gwilczyk.github@gmail.com>
2020-12-17 11:58:32 +0100

    Setting env variables & config with Next.js

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "799ca7ab3146c4b8ddda";
+/******/ 	var hotCurrentHash = "4e7941708a443fbc0b01";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
